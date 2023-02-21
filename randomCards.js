e98e9a9cf9753b925401e7f8bc188f6386d84846
UCCS-MDelgado <mdelgad3@uccs.edu>
2023-02-21 11:33:00 -0700

    Starting player hand created

--- randomCards.js
+++ randomCards.js
@@ -1,5 +1,13 @@
 let deckSize = 60
+let handSize = 5
 
-let deckArray = Array.from({length: deckSize}, () => Math.floor(Math.random() * deckSize));
+let deckArray = Array.from({ length: deckSize }, () => Math.floor(Math.random() * deckSize));
+
+let handArray = []
+
+for (let i = 0; i < handSize; i++) {
+    handArray.push(deckArray[i])
+}
 
 console.log(deckArray)
+console.log(handArray)
