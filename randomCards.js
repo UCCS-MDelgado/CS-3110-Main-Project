757194ae23768fd82ecb29e554da4ec6441bb457
UCCS-MDelgado <mdelgad3@uccs.edu>
2023-02-21 12:03:48 -0700

    Added function to draw card

--- randomCards.js
+++ randomCards.js
@@ -1,13 +1,30 @@
 let DECK_SIZE = 60
 let HAND_SIZE = 5
 
+function drawCard(deck, hand) {
+    let newCard = deck.shift()
+    hand.push(newCard)
+
+    console.log("Deck after draw")
+    console.log(deck)
+
+    console.log("Hand after draw")
+    console.log(hand)
+}
+
 let deckArray = Array.from({ length: DECK_SIZE }, () => Math.floor(Math.random() * DECK_SIZE));
+
+console.log("Initial Deck")
+console.log(deckArray)
 
 let handArray = []
 
 for (let i = 0; i < HAND_SIZE; i++) {
-    handArray.push(deckArray[i])
+    handArray.push(deckArray.shift())
 }
 
-console.log(deckArray)
+console.log("Cards in Hand")
 console.log(handArray)
+
+console.log("Cards Left in Deck")
+console.log(deckArray)
