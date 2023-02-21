14f4ac5c97fbe9c892e3c13ad22ec51be50547b7
UCCS-MDelgado <mdelgad3@uccs.edu>
2023-02-21 11:38:52 -0700

    Properly formatted constants

--- randomCards.js
+++ randomCards.js
@@ -1,11 +1,11 @@
-let deckSize = 60
-let handSize = 5
+let DECK_SIZE = 60
+let HAND_SIZE = 5
 
-let deckArray = Array.from({ length: deckSize }, () => Math.floor(Math.random() * deckSize));
+let deckArray = Array.from({ length: DECK_SIZE }, () => Math.floor(Math.random() * DECK_SIZE));
 
 let handArray = []
 
-for (let i = 0; i < handSize; i++) {
+for (let i = 0; i < HAND_SIZE; i++) {
     handArray.push(deckArray[i])
 }
 
