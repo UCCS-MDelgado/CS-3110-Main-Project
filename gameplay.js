d2147930fef7a19c345b6d5e95023e3dfe67535a
UCCS-MDelgado <mdelgad3@uccs.edu>
2023-02-21 14:59:46 -0700

    Moved all gameplay to new JS file

---
+++ gameplay.js
@@ -0,0 +1,31 @@
+import { drawCards } from "cardFunctions.js"
+import { shuffleDeck } from "cardFunctions.js"
+import { shuffleHand } from "cardFunctions.js"
+import { moveIntoPlay } from "cardFunctions.js"
+import { discardFromPlay } from "cardFunctions.js"
+import { discardToHand } from "cardFunctions.js"
+import { discardToDeck } from "cardFunctions.js"
+
+let DECK_SIZE = 60
+let INITIAL_HAND_SIZE = 5
+
+let deckArray = [], handArray = [], playField = [], discardArray = []
+
+let nums = []
+
+for (let i = 1; i <= DECK_SIZE; i++) {
+    nums.push(i)
+}
+let i = nums.length,
+    j = 0;
+
+while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    deckArray.push(nums[j]);
+    nums.splice(j, 1);
+}
+
+console.log("Initial Deck")
+console.log(deckArray)
+
+drawCards(deckArray, handArray, INITIAL_HAND_SIZE)
